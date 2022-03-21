76b6a2018c096b50865b14947ecfae7adea2604d
Michael Yocca <mjyocca@gmail.com>
2022-03-21 16:34:49 -0700

    fix test failures

--- ruby-on-rails-demo-blog/app/models/post.rb
+++ ruby-on-rails-demo-blog/app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
     validates_presence_of :title
-    validates_presence_of :content
     has_rich_text :content
-    has_many :comments
+    has_many :comments, dependent: :destroy
 end
